7beb283aef7eb753c245e6d13396bca31334f3c1
NaumanBajwa-BayArea <drnaumanbajwadds@gmail.com>
2019-12-16 13:42:59 -0800

    added gitignore file

---
+++ DMV QUIZ/Model/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  DMV QUIZ
+//
+//  Created by Nauman Bajwa on 12/14/19.
+//  Copyright © 2019 Nauman Bajwa. All rights reserved.
+//
+
+import Foundation
+
+class Question{
+    
+    var questionText : String = ""
+    var answerOptionsArray : Array<String> = [String]()
+    var correctAnswerText : String = ""
+}
---
+++ DMV QUIZ/Model/QuizModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizModel.swift
+//  DMV QUIZ
+//
+//  Created by Nauman Bajwa on 12/14/19.
+//  Copyright © 2019 Nauman Bajwa. All rights reserved.
+//
+
+import Foundation
+
+class QuizModel{
+    var question : Question  = Question()
+
+        
+}
